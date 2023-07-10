9fdfa9011048647345cc98ce82e23f98b9c2d769
r.lepeha <r.lepeha@traderevolution.com>
2023-07-10 23:39:00 +0300

    10 jul 2023

--- other_color_pick/src/main/java/ua/com/compose/other_color_pick/data/ColorDatabase.kt
+++ other_color_pick/src/main/java/ua/com/compose/other_color_pick/data/ColorDatabase.kt
@@ -12,10 +12,12 @@ class ColorDatabase(context: Context) {
     companion object {
         val MIGRATION_1_2: Migration = object : Migration(1, 2) {
             override fun migrate(database: SupportSQLiteDatabase) {
-                val date = java.text.DateFormat.getDateInstance(2, Locale.getDefault()).format(this)
-
+                val date = java.text.DateFormat.getDateInstance(2, Locale.getDefault()).format(Date())
+                database.execSQL("CREATE TABLE IF NOT EXISTS `colors` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `color` INTEGER NOT NULL DEFAULT -1, `palletId` INTEGER NOT NULL DEFAULT -1)")
+                database.execSQL("CREATE TABLE IF NOT EXISTS `pallets` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` TEXT NOT NULL DEFAULT '')")
                 database.execSQL("INSERT INTO colors (id, color, palletId) SELECT id, color, 0 FROM colorItem")
                 database.execSQL("INSERT INTO pallets (id, name) VALUES (${0}, '${date}')")
+                database.execSQL("DROP TABLE colorItem");
             }
         }
     }
